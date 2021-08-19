9fdd216ed6b5ce9abbaa28252b6b4bbd0763e378
Jose Salas <fsalas.y@gmail.com>
2021-08-19 17:43:58 -0500

    Add Ads
    this is the initialization of the application, along with the initialization of the ads.

---
+++ franee/franee/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+using MarcTron.Plugin;
+
+namespace franee
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CrossMTAdmob.Current.OnRewardedVideoAdLoaded += (s, args) =>
+            {
+                CrossMTAdmob.Current.ShowRewardedVideo();
+            };
+
+            CrossMTAdmob.Current.OnInterstitialLoaded += (s, args) =>
+            {
+                CrossMTAdmob.Current.ShowInterstitial();
+            };
+        }
+
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            CrossMTAdmob.Current.LoadRewardedVideo("ca-app-pub-1430833140613652/7977366946");
+        }
+
+
+        void Button_Clicked_1(object sender, System.EventArgs e)
+        {
+            CrossMTAdmob.Current.LoadInterstitial("ca-app-pub-1430833140613652/1911292671");
+        }
+    }
+}
